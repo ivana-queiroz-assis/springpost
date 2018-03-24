d72fc964a8a30b3d08d0d503aa5c066d30e30230
ivana-queiroz-assis <ivanaqueirozassis@hotmail.com>
2018-03-24 12:41:11 -0300

    Iniciando projeto.

---
+++ src/main/java/br/com/ivana/springPost/SpringPostApplication.java
@@ -0,0 +1,12 @@
+package br.com.ivana.springPost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPostApplication.class, args);
+	}
+}
